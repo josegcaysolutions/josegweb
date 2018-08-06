38665a608c8ec273926a1c9a9a7092bdbe674587
Jose Gonzalez <gonzalez@caysolutions.de>
2018-08-06 12:40:24 +0200

    commit

---
+++ application/controllers/Joseg.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Joseg extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('template/header');
+		$this->load->view('modulos/index');
+		$this->load->view('template/footer');
+	}
+}
